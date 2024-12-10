8358d24f165e2b2c542a1cf332d9f2dc5aee2486
ghNazim <160389831+ghNazim@users.noreply.github.com>
2024-12-10 20:05:08 +0530

    basic routes done

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
 
-const mongoURI = process.env.MONGO_URI;
+
 mongoose
-  .connect(mongoURI)
-  .then(() => {
-    console.log("connected to mongoDB");
-  })
-  .catch(() => {
-    console.log("could not connect to database");
-  });
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
 const app = express();
-app.use(cors({ credentials: true }));
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 
-const port = process.env.PORT || 5500;
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/search", shopSearchRouter);
 
-app.listen(port, () => {
-  console.log("Server is running on port: " + port);
-});
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
